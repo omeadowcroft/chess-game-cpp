6d2a5859d52afeb795d8ca4025ceb4370d2a5152
oscar <meadowcroftoscar@gmail.com>
2026-04-03 16:46:59 +0100

    Add click -> square mapping and test messages in console. Add coordinates.cpp to makefile

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ CXX = g++
 CXXFLAGS = -std=c++17
 LIBS = -lsfml-graphics -lsfml-window -lsfml-system
 
-chess: main.cpp board.cpp renderer.cpp
-	$(CXX) $(CXXFLAGS) main.cpp board.cpp renderer.cpp -o chess $(LIBS)
+chess: main.cpp board.cpp renderer.cpp coordinates.cpp
+	$(CXX) $(CXXFLAGS) main.cpp board.cpp renderer.cpp coordinates.cpp -o chess $(LIBS)
 
 clean:
 	rm -f chess
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "board.h"
 #include "renderer.h"
+#include "coordinates.h"
 
 int main(){
 
@@ -9,20 +11,31 @@ int main(){
   loadTextures();
 
   while (window.isOpen()) {
-    while (const std::optional event = window.pollEvent()) {
-      if (event->is<sf::Event::Closed>())
-        window.close();
-      if (event->is<sf::Event::Resized>())
-        window.setView(sf::View(sf::FloatRect({0,0},{(float)window.getSize().x,(float)window.getSize().y})));
-    }
-
-    window.clear(sf::Color(26,31,46));
-
     int windowWidth = window.getSize().x;
     int windowHeight = window.getSize().y;
     int boardSize = std::min(windowWidth,windowHeight) * 0.9;
     int boardStartX = (windowWidth - boardSize) / 2;
     int boardStartY = (windowHeight - boardSize) / 2;
+    float squareSize = boardSize/8.f;
+
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>())
+        window.close();
+      if (event->is<sf::Event::Resized>())
+        window.setView(sf::View(sf::FloatRect({0,0},{(float)window.getSize().x,(float)window.getSize().y})));
+      if (const auto* click = event->getIf<sf::Event::MouseButtonPressed>()) {
+        if (click->button == sf::Mouse::Button::Left) {
+          int col = (click->position.x - boardStartX) / squareSize;
+          int row = (click->position.y - boardStartY) / squareSize;
+
+          if (col >= 0 && col < 8 && row >= 0 && row < 8) {
+            std::cout << colToFile(col) << rowToRank(row) << std::endl;
+          }
+        }
+      }
+    }
+
+    window.clear(sf::Color(26,31,46));
 
     sf::Color darkColour(43,62,44);
     sf::Color lightColour(179,200,170);
@@ -33,8 +46,6 @@ int main(){
  *  choose colour based off row and col
  *  draw sqare at start + [columns*size,rows*size]
 */
-
-    float squareSize = boardSize/8.f;
     sf::RectangleShape square({squareSize,squareSize});
     square.setPosition({(float)boardStartX, (float)boardStartY});
     for(int row=0;row<8;row++){
